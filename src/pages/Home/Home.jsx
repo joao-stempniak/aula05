889e5605e768d1d86c8d89e37dfd9d11bcc559dc
joao Stempniak <joao48991884694@gmail.com>
2024-08-22 21:36:21 -0300

    segundo commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,42 @@
+import enderecoImg from '../../../public/img/image.jpg'
+import {Footer} from "../../components/Footer"
+
+import './styles.css'
 
 
-export default function App() {
-  
+export function Home() {
+
 
   return (
-    <h1>pagina principal</h1>
-    
+    <>
+      <img src={ enderecoImg } alt="Descrição da img"/>
+
+
+      <h1><span class="sombra">Transformando </span><span class="coloridoazul">espaço</span>,
+      <br/> criando <span class="coloridoazul">história</span> </h1>
+
+
+      <p>
+        Nós criamos ambientes  <b> exclusívos </b> e <b> únicos </b>, com muito bom gosto e profissionalismo, <br/> investimos tempo e
+          <span class="coloridoazul"> dedicação no seu projeto</span>. Nosso compromisso vai além de simplesmente decorar.
+      </p>
+
+
+      <br/>
+      <br/>
+      <p>
+          Tenha <span class="coloridoazul">ambientes limpos</span> e bem decorados, transmitindo <b>elegância</b> e <b> finesse </b>para todo <br/> aquele que estiver ali. Sua história visual começa aqui!
+      </p>
+      <Footer/>
+     
+      <footer>
+          <div class="linha" ></div>            
+          <a href="instagram.com" class="coloridoazul" id="insta">Instagram</a>
+          &nbsp;&nbsp;&nbsp;&nbsp;          
+          <a href="whatsapp.com" class="coloridoazul">Fale Conosco</a>    
+      </footer>
+
+
+    </>
   )
 }
-
-
